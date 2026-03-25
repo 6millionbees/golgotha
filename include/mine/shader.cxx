ec4f15e8f13cfe2130c1a2f747e15df8f6bd5cd6
6millionbees <ardenboettcher@gmail.com>
2026-03-25 16:15:38 -0400

    initial commit

---
+++ include/mine/shader.cxx
@@ -0,0 +1,14 @@
+#include "shader_s.h"
+
+#include <glad/glad.h>
+#include <glm/glm.hpp>
+#include <glm/gtc/matrix_transform.hpp>
+#include <glm/gtc/type_ptr.hpp>
+
+#include <string>
+#include <fstream>
+#include <sstream>
+#include <iostream>
+
+
+
---
+++ main.cxx
@@ -0,0 +1,27 @@
+// Arden Boettcher
+// Started: 3/23/26
+// Version: 0.0.1
+// Golgotha
+
+// OpenGL loaders
+#include <glad/glad.h>
+#include <GLFW/glfw3.h>
+
+// Math <3
+#include <glm/glm.hpp>
+#include <glm/gtc/matrix_transform.hpp>
+#include <glm/gtc/type_ptr.hpp>
+#include <cmath>
+
+// Iostream
+#include <iostream>
+
+// My Stuff :)
+#include "shader.h"
+#include "resource_manager.h"
+
+
+int main()
+{
+	std::cout << "Hello World" << std::endl;
+}
